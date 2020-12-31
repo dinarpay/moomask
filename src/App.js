2631ee48e3cd4ac47c94cab53e26570b52bf334a
Kamaljeet Singh <kalpanassleep@gmail.com>
2020-12-31 08:31:32 +0530

    moving to react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+import Header from './components/header'
+import CreateWallet from './pages/create-wallet'
+import ImportWallet from './pages/import-wallet'
+import Signin from './pages/signin'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+import Container from '@material-ui/core/Container';
+import ConnectionProvider from './providers/connection-context';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(',')
+  },
+  palette: {
+    primary: {
+      main: '#184c82'
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <ConnectionProvider>
+        <Router>
+          <Container>
+            <Header />
+            <Switch>
+              
+              <Route path="/create-wallet">
+                <CreateWallet />
+              </Route>
+              <Route path="/import-wallet">
+                <ImportWallet />
+              </Route>
+              <Route path="/signin">
+                <Signin />
+              </Route>
+              <Route exact path="/">
+                <Signin />
+              </Route>
+            </Switch>
+          </Container>
+        </Router>
+      </ConnectionProvider>
+    </ThemeProvider>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { RecoilRoot } from "recoil";
+import recoilPersist from 'recoil-persist'
+
+const { RecoilPersist, updateState } = recoilPersist([], {
+  key: 'moomask-persist', // this key is using to store data in local storage
+  storage: localStorage
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot initializeState={({set}) => {
+       updateState({set}) 
+    }}>
+      <RecoilPersist />
+      <App />
+    </RecoilRoot>  
   </React.StrictMode>,
   document.getElementById('root')
-);
-
+  );
+  
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
